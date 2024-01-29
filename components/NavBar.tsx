3294e6f96588e4bb3705394f5e5355df9ecb2c88
josuerushanika <josuerushanika@gmail.com>
2024-01-29 17:56:26 +0200

    Update styles

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,15 +4,15 @@ import React from "react";
 
 function NavBar() {
     return (
-        <><div>
-            <div>
+        <><div className='flex'>
+            <div className='flex gap-10 items-center'>
                 <Image src='/logo.png'
                     alt='logo'
                     width={120}
                     height={600} />
             </div>
 
-            <div className='flex'>
+            <div className='flex gap-6'>
                 <h2> Home</h2>
                 <h2> Hystory </h2>
                 <h2> Help </h2>
