e10e7cc13aa4550e5959ebde52f3082875d1d935
josuerushanika <josuerushanika@gmail.com>
2024-01-29 12:56:44 +0200

    Add user button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
+     <UserButton afterSignOutUrl="/" />
      <h2> Subscribe to my channel</h2>
    </div>
   );
