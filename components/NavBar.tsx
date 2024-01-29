a5cae4534c79bf5b60252d5cb60a4fd8bede206f
josuerushanika <josuerushanika@gmail.com>
2024-01-29 17:51:37 +0200

    Update files

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,22 +4,20 @@ import React from "react";
 
 function NavBar() {
     return (
-        <div>
+        <><div>
             <div>
                 <Image src='/logo.png'
                     alt='logo'
                     width={120}
-                    height={600}
-                />
+                    height={600} />
             </div>
 
             <div className='flex'>
-                   <h2> Home</h2>
-                   <h2> Hystory </h2>
-                   <h2> Help </h2>
+                <h2> Home</h2>
+                <h2> Hystory </h2>
+                <h2> Help </h2>
             </div>
-        </div>
-        <UserButton afterSignOutUrl="/" />
+        </div><UserButton afterSignOutUrl="/" /></>
     );
 }
 
