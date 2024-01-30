0776117d65f123f85dffbf325799984d545c723b
josuerushanika <josuerushanika@gmail.com>
2024-01-30 15:15:11 +0200

    Update files

--- components/booking/AutoCompleteAdress.tsx
+++ components/booking/AutoCompleteAdress.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 function AutoCompleteAdress() {
-  return (
-    <div>AutoCompleteAdress</div>
-  )
+    return (
+        <>
+            <div className='mt-5'>
+                <div>
+                    <label className='text-gray-400'>  Where from  </label>
+                    <input type="text" className='bg-white p-1 border-[1px] w-full rounded-md outline-none' />
+
+                </div>
+            </div>
+
+            <div className='mt-3'>
+                <div>
+                    <label className='text-gray-400'>  Where To  </label>
+                    <input type="text" className='bg-white p-1 border-[1px] w-full rounded-md outline-none' />
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default AutoCompleteAdress
--- components/booking/Booking.tsx
+++ components/booking/Booking.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import AutoCompleteAdress from './AutoCompleteAdress'
 
 function Booking() {
-  return (
-    <div> Booking Component</div>
-  )
+    return (
+        <div className='p-5'>
+            <h2 className='text-[20px] font-semibold'>Booking</h2>
+            <div className='border-[1px] p-5 rounded-md'>
+                <AutoCompleteAdress />
+            </div>
+        </div>
+    )
 }
 
 export default Booking
