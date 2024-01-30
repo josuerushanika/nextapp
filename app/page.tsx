bf5d5ef7f2892d393eb2b527a4a69ebb02e26dbb
josuerushanika <josuerushanika@gmail.com>
2024-01-30 13:06:51 +0200

    Update styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="grid  grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-3">
         <div className="bg-blue-100">
          Booking
         </div>
-        <div className="col-span-2 bg-red-200">
+        <div className="col-span-2 bg-red-200 ">
          Map
         </div>
       </div>
