40096f9351b85dbe32fb798901873d4c4e488775
josuerushanika <josuerushanika@gmail.com>
2024-02-01 19:06:39 +0200

    add styles

--- components/booking/AutoCompleteAdress.tsx
+++ components/booking/AutoCompleteAdress.tsx
@@ -5,7 +5,7 @@ function AutoCompleteAdress() {
         <>
             <div className='mt-5'>
                 <div>
-                    <label className='text-gray-400'>  Where from  </label>
+                    <label className= 'text-gray-400'>  Where from  </label>
                     <input type="text" className='bg-white p-1 border-[1px] w-full rounded-md outline-none' />
 
                 </div>
